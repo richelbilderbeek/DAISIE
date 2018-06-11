7b95ca01af243bde190c09b971a7988442b2a1ad
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-11 14:12:36 +0200

    Put in date

--- DAISIE/vignettes/demo_sim.R
+++ DAISIE/vignettes/demo_sim.R
@@ -8,31 +8,6 @@ knitr::opts_chunk$set(
 library(DAISIE)
 
 ## ------------------------------------------------------------------------
-data(Galapagos_datatable)
-knitr::kable(Galapagos_datatable)
-
-## ------------------------------------------------------------------------
-data(Galapagos_datatable) 
-      
-Galapagos_datalist <- DAISIE_dataprep( 
-  datatable = Galapagos_datatable, 
-  island_age = 4, 
-  M = 1000
-)
-
-## ------------------------------------------------------------------------
-data(Galapagos_datatable) 
-
-Galapagos_datalist_2types <- DAISIE_dataprep( 
-  datatable = Galapagos_datatable, 
-  island_age = 4, 
-  M = 1000, 
-  number_clade_types = 2, 
-  list_type2_clades = "Finches", 
-  prop_type2_pool = 0.163
-)
-
-## ------------------------------------------------------------------------
 pars = c(2.550687345,2.683454548,Inf,0.00933207,1.010073119)
 
 island_replicates = DAISIE_sim( 
