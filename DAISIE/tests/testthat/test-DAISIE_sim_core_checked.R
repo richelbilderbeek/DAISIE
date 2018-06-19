05429e6addc76739d7a9fd9e24807136188983ea
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-19 11:27:49 +0200

    Plot 'DAISIE_sim_core_checked'

--- DAISIE/tests/testthat/test-DAISIE_sim_core_checked.R
+++ DAISIE/tests/testthat/test-DAISIE_sim_core_checked.R
@@ -3,11 +3,13 @@ context("DAISIE_sim_core_checked")
 test_that("use", {
 
   set.seed(42)
+  n_mainland_species <- 1
+  sim_time <- 10
   result <- NULL
   expect_silent(
     result <- DAISIE::DAISIE_sim_core_checked(
-      sim_time = 10, 
-      n_mainland_species = 1, 
+      sim_time = sim_time, 
+      n_mainland_species = n_mainland_species, 
       clado_rate = 1.0, 
       ext_rate = 0.1,
       carr_cap = 4,
@@ -19,5 +21,15 @@ test_that("use", {
   expect_true("branching_times" %in% names(result))
   expect_true("stac" %in% names(result))
   expect_true("missing_species" %in% names(result))
+  island_replicates <- list()
+  island_replicates[[1]][[1]] <- result
   # May also return 'other_clades_same_ancestor'
+  island_replicates <- DAISIE_format_CS(
+    island_replicates = island_replicates,
+    time = sim_time,
+    M = n_mainland_species,
+    sample_freq = 25,
+    verbose = FALSE
+  )
+  DAISIE_plot_sims(island_replicates, use_dev_new = FALSE)
 })
