4e3bd1581e7b8e77ec8e04210b4602f8236f8014
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-26 08:13:26 +0200

    Add example to doc

--- DAISIE/R/DAISIE_get_brts_mya.R
+++ DAISIE/R/DAISIE_get_brts_mya.R
@@ -2,6 +2,11 @@
 #' from a data table
 #' @param data_table data table
 #' @return the sorted branching times, in million years ago
+#' @examples 
+#'   data(Galapagos_datatable)
+#'   brts_mya <- DAISIE_get_brts_mya(data_table = Galapagos_datatable)
+#'   testit::assert(length(brts_mya) > 1)
+#'   testit::assert(all(brts_mya > 0))
 #' @author Richel J.C. Bilderbeek
 DAISIE_get_brts_mya <- function(data_table) {
   testit::assert("Branching_times" %in% names(data_table))
