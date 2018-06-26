a24be6f76cbf78a11d6b8063032aac2bcbf55b8a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-26 07:04:01 +0200

    Roxygenize 'DAISIE_plot_sim '

--- DAISIE/R/DAISIE_plot_sims.R
+++ DAISIE/R/DAISIE_plot_sims.R
@@ -1,3 +1,38 @@
+#' Plot island species-through-time (STT) plots
+#' 
+#' Produces STT plots. If only one type of species is present in the simulated
+#' islands, STT is plotted for all species. If two types are present, three
+#' plots are produced: STT for all, STT for type 1 and STT for type 2.
+#' 
+#' R plots with number of total, endemic and non-endemic STTs for different
+#' types of species for the entire time span the islands were simulated.
+#' 2.5-97.5th percentiles are plotted in light grey, 25-75th percentiles
+#' plotted in dark grey.
+#' 
+#' @param island_replicates Island replicates in DAISIE format (Produced in
+#' DAISIE_sim with format=TRUE option, or in DAISIE_format_sim)
+#' @param use_dev_new Default=TRUE. Use \code{dev.new} before plotting. Set to
+#' \code{FALSE} when plotting within a vignette
+#' @return R plot.
+#' @author Luis Valente
+#' @seealso \code{\link{DAISIE_sim}} \code{\link{DAISIE_format_CS}}
+#' @references Valente, L.M., A.B. Phillimore and R.S. Etienne (2015).
+#' Equilibrium and non-equilibrium dynamics simultaneously operate in the
+#' Galapagos islands. Ecology Letters 18: 844-852.
+#' @keywords models
+#' @examples
+#'  
+#' 
+#' ### Plot islands with single process (only one type of species)
+#' data(islands_1type_1000reps)
+#' DAISIE_plot_sims(island_replicates = islands_1type_1000reps)
+#' 
+#' 
+#' ### Plot island with type 1 and type 2
+#' data(islands_2types_1000reps)
+#' DAISIE_plot_sims(island_replicates = islands_2types_1000reps)
+#' 
+#' 
 DAISIE_plot_sims <- function(
   island_replicates, 
   use_dev_new = TRUE
