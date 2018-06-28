5979de82a1380b08a3b3bf68a577f4670a6549a2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-28 15:01:15 +0200

    Removed link to absent 'DAISIE_format_sim' function from doc, added 'DAISIE_plot_stt'

--- DAISIE/tests/testthat/test-DAISIE_sim_with_mainland.R
+++ DAISIE/tests/testthat/test-DAISIE_sim_with_mainland.R
@@ -39,7 +39,9 @@ test_that("Without mainland processes, should do the same as classic model", {
     replicates = n_replicates
   )
   
-  # Compare
+  # Compare for being exactly the same
+  # This test will soften up in the future.
+  # In the future, the STT's are compared to be equivalent
   testit::assert(n_replicates == length(created))
   expect_equal(length(expected), length(created))
   for (replicate_index in seq_along(expected)) {
@@ -53,4 +55,39 @@ test_that("Without mainland processes, should do the same as classic model", {
       expect_equal(expected_element, created_element)
     }
   }
+})
+
+test_that("can plot", {
+  # Create parameters
+  sim_time <- 10
+  n_mainland_species <- 10
+  clado_rate <- 1.0
+  ext_rate <- 0.1
+  carr_cap <- 10
+  imm_rate <- 1.0
+  ana_rate <- 1.0
+  n_replicates <- 3
+  seed <- 42
+  testit::assert(sim_time > 0.0)
+  testit::assert(n_mainland_species > 0)
+  testit::assert(clado_rate >= 0.0)
+  testit::assert(ext_rate >= 0.0)
+  testit::assert(carr_cap > 0)
+  testit::assert(imm_rate > 0.0)
+  testit::assert(ana_rate >= 0.0)
+  testit::assert(n_replicates > 0)
+  pars <- c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate)
+  
+  set.seed(seed)
+  results <- DAISIE_sim_with_mainland(
+    time = sim_time,
+    M = n_mainland_species,
+    pars = pars,
+    replicates = n_replicates
+  )
+  DAISIE_plot_sims(
+    island_replicates = results, 
+    use_dev_new = FALSE, 
+    plot_plus_one = FALSE
+  )
 })
