d525b980f6efebc4e8305a67668baa560a3947e6
Neves-P <p.m.santos.neves@rug.nl>
2018-08-08 17:50:18 +0200

    Moved newest test to correct test folder (overwriting the old tests)

--- DAISIE/tests/testthat/test-DAISIE_sim_core.R
+++ DAISIE/tests/testthat/test-DAISIE_sim_core.R
@@ -34,9 +34,8 @@ test_that("new and v1.4 should give same results", {
   expect_true(length(new$other_clades_same_ancestor) == length(old$other_clades_same_ancestor))
   expect_true(new$other_clades_same_ancestor[[1]]$species_type == old$other_clades_same_ancestor[[1]]$species_type)
 
-  # stt_table, branching_times, missing species are different
-  expect_equal(new$stt_table, old$stt_table)
-  expect_equal(new$branching_times, old$branching_times)
+  expect_true(all(new$stt_table == old$stt_table))
+  expect_true(all(new$branching_times == old$branching_times))
   expect_true(new$other_clades_same_ancestor[[1]]$brts_miss == old$other_clades_same_ancestor[[1]]$brts_miss)
 })
 
@@ -60,35 +59,3 @@ test_that("Clean run should be silent", {
   )
 
 })
-
-test_that("Pedro's should run silent", {
-
-  skip("WIP")
-  set.seed(234567890)
-  DAISIE_sim_core(
-    time = 10,
-    mainland_n = 1,
-    pars = c(2.5, 2.2, 10, 0.009, 1.01),
-    Apars = create_area_params(
-      max_area = 10,
-      proportional_peak_t = 0.5,
-      peak_sharpness = 1,
-      total_island_age = 5
-    ), 
-    Epars = c(1.7, 100),
-    island_ontogeny = "quadratic"
-  )
-
-  expect_silent(
-    DAISIE_sim_core(
-      time = 10,
-      mainland_n = 1,
-      pars = c(2.5, 2.2, 10, 0.009, 1.01),
-      Apars = create_area_params(5000, 0.2, 1, 15),
-      Epars = c(1.7, 100),
-      island_ontogeny = "quadratic"
-    )
-  )
-
-})
-
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(DAISIE)
-
-test_check("DAISIE")
