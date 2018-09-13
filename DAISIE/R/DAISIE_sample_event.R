cc7356c291e920ef39d7bcdd7db816402d228999
Neves-P <10762799+Neves-P@users.noreply.github.com>
2018-09-14 00:51:03 +0200

    Convert code to sample event function

---
+++ DAISIE/R/DAISIE_sample_event.R
@@ -0,0 +1,44 @@
+#' Samples what event to happen next
+#'
+#' @param rates numeric list with probability rates for each event. In the 
+#' ontogeny case it also contains the maximum possible probability for the 
+#' event at each timestep.
+#'
+#' @return numeric indicating what event will happen, or a supposed event that would
+#' happen in some timesteps of the ontogeny algorithm.
+#' #' \itemize{
+#'   \item{[1]: immigration event}
+#'   \item{[2]: extinction event}
+#'   \item{[3]: cladogenesis event}
+#'   \item{[4]: anagenesis event}
+#'   \item{[5]: proposed extinction that will not happen}
+#'   \item{[6]: proposed immigration that will not happen}
+#'   \item{[7]: proposed cladogenesis that will not happen}
+#' }
+#' @author Pedro Neves
+determine_event <- function(rates, island_ontogeny) {
+  # If statement prevents odd behaviour of sample when rates are 0
+  if (is.null(island_ontogeny)) {
+    possible_event <- sample(1:4, 1, prob = c(rates$immig_rate,
+                                              rates$ext_rate,
+                                              rates$ana_rate,
+                                              rates$clado_rate), 
+                             replace = FALSE)
+  } else {
+    testit::assert(are_rates(rates))
+    possible_event <- sample(1:7, 1, prob = c(
+      rates$immig_rate,
+      rates$ext_rate,
+      rates$ana_rate,
+      rates$clado_rate,
+      (rates$ext_rate_max - rates$ext_rate),
+      (rates$immig_rate_max - rates$immig_rate),
+      (rates$clado_rate_max - rates$clado_rate)),
+      replace = FALSE)
+    
+  }
+  
+    
+  possible_event
+}
+
