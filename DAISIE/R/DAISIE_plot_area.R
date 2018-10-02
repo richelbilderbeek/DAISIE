5ee01285edddf5c3ca2da649f01f2003d0634035
Neves-P <p.m.santos.neves@rug.nl>
2018-10-02 17:26:45 +0200

    Fix rates plotting functions. Update doc

--- DAISIE/R/DAISIE_plot_area.R
+++ DAISIE/R/DAISIE_plot_area.R
@@ -23,8 +23,8 @@
 DAISIE_plot_area <- function(totaltime,
                              Apars,
                              island_ontogeny = "quadratic",
-                             resolution
-) {
+                             resolution) {
+  
   testit::assert(are_area_params(Apars))
   
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
@@ -56,32 +56,52 @@ DAISIE_plot_area <- function(totaltime,
 
 #' Plots extinction rate function through time
 #'
-#' @param island_area_time something
-#' @param totaltime something
-#' @param K something
-#' @param Apars something
-#' @param Epars something
-#' @param island_ontogeny something
-#' @param removed_timepoints something
+#' @param totaltime total time of simulation
+#' @param K K (clade-level carrying capacity)
+#' @param Apars a numeric vector:
+#' \itemize{
+#'   \item{[1]: maximum area}
+#'   \item{[2]: value from 0 to 1 indicating where in the island's history the 
+#'   peak area is achieved}
+#'   \item{[3]: sharpness of peak}
+#'   \item{[4]: total island age}
+#' }
+#' @param Epars a numeric vector:
+#' \itemize{
+#'   \item{[1]: minimum extinction when area is at peak}
+#'   \item{[2]: extinction rate when current area is 0.10 of maximum area}
+#' }
+#' @param island_ontogeny a string describing the type of island ontogeny. Can be \code{NULL},
+#' \code{quadratic} for a beta function describing area through time,
+#'  or \code{linear} for a linear function
+#' @param removed_timepoints starting position of time vector
+#' @param resolution resolution of time axis
 #'
-#' @return extinction rate through time plot
+#' @author Pedro Neves
+#' @return extinction rate through time plot and dataframe with extinction 
+#' at corresponding time
 #' @export
-DAISIE_plot_extinction <- function(island_area_time,
-                                   totaltime,
+DAISIE_plot_extinction <- function(totaltime,
                                    K, 
                                    Apars, 
                                    Epars, 
                                    island_ontogeny = "quadratic", 
-                                   removed_timepoints) {
+                                   removed_timepoints,
+                                   resolution) {
   
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed for this function to work. Please install it.",
          call. = FALSE)
   }
   
+  
+  axis <- seq(0, totaltime, by = resolution)
+
+  
+  
   ext_rate <- c()
-  for (i in seq_along(island_area_time$Time)) {
-    ext_rate[i] <- DAISIE::get_ext_rate(timeval = island_area_time$Time[i],
+  for (i in seq_along(axis)) {
+    ext_rate[i] <- DAISIE::get_ext_rate(timeval = axis[i],
                                    totaltime = totaltime,
                                    Apars = Apars,
                                    Epars = Epars,
@@ -93,27 +113,38 @@ DAISIE_plot_extinction <- function(island_area_time,
                                    )
   }
   
-  ext_rate_time <- data.frame(Extinction = ext_rate[removed_timepoints:length(ext_rate)], Time = island_area_time$Time[removed_timepoints:length(island_area_time$Time)])
+  ext_rate_time <- data.frame(Extinction = ext_rate[removed_timepoints:length(ext_rate)], Time = axis[removed_timepoints:length(axis)])
   
   Time <- NULL; rm(Time) # nolint, fixes warning: no visible binding for global variable
   Extinction <- NULL; rm(Extinction) # nolint, fixes warning: no visible binding for global variable
-  ggplot2::ggplot(data = ext_rate_time, ggplot2::aes(x = Time, y = Extinction)) +
+  plot(ggplot2::ggplot(data = ext_rate_time, ggplot2::aes(x = Time, y = Extinction)) +
     ggplot2::geom_line(size = 1) +
-    ggplot2::ylim(0, 10)
+    ggplot2::ylim(0, 5))
   invisible(ext_rate_time)
 }
 
 #' Plot immigration rate through time
 #'
-#' @param totaltime Something
-#' @param K Something
-#' @param Apars Something
-#' @param gam Something
-#' @param mainland_n Something
-#' @param island_ontogeny Something
-#' @param removed_timepoints Something
-#' @param immig_rate Something
-#'
+#' @param totaltime total time of simulation
+#' @param K K (clade-level carrying capacity)
+#' @param Apars a numeric vector:
+#' \itemize{
+#'   \item{[1]: maximum area}
+#'   \item{[2]: value from 0 to 1 indicating where in the island's history the 
+#'   peak area is achieved}
+#'   \item{[3]: sharpness of peak}
+#'   \item{[4]: total island age}
+#' }
+#' @param gam minimum per capita immigration rate
+#' @param mainland_n number of mainland species. Set as 1 for clade-specific 
+#' diversity dependence
+#' @param island_ontogeny a string describing the type of island ontogeny. Can be \code{NULL},
+#' \code{quadratic} for a beta function describing area through time,
+#'  or \code{linear} for a linear function
+#' @param removed_timepoints starting position of time vector
+#' @param resolution resolution of time axis
+#' 
+#' @author Pedro Neves
 #' @return a plot with immigration rate through time
 #' @export
 #'
@@ -124,33 +155,36 @@ DAISIE_plot_immigration <- function(totaltime,
                                     mainland_n,
                                     island_ontogeny = "quadratic", 
                                     removed_timepoints,
-                                    immig_rate) {
+                                    resolution) {
   
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed for this function to work. Please install it.",
          call. = FALSE)
   }
-  island_area_time <- seq(0, totaltime, by = 0.001)
+  
+  
+  axis <- seq(0, totaltime, by = resolution)
+
   immig_rate <- c()
-  for (i in seq_along(island_area_time)) {
-    immig_rate[i] <- get_immig_rate(timeval = island_area_time[i],
-                                  totaltime = totaltime,
-                                  Apars = Apars,
-                                  gam = gam, 
-                                  K = K, 
-                                  mainland_n = 1000, 
-                                  island_spec = matrix(ncol = 1),
-                                  island_ontogeny = island_ontogeny
+  for (i in seq_along(axis)) {
+    immig_rate[i] <- get_immig_rate(timeval = axis[i],
+                                    totaltime = totaltime,
+                                    Apars = Apars,
+                                    gam = gam, 
+                                    K = K, 
+                                    mainland_n = 1, 
+                                    island_spec = matrix(ncol = 1),
+                                    island_ontogeny = island_ontogeny
     )
   }
   
-  immig_rate_time <- data.frame(Immigration = immig_rate[removed_timepoints:length(immig_rate)], Time = island_area_time[removed_timepoints:length(island_area_time)])
+  immig_rate_time <- data.frame(Immigration = immig_rate[removed_timepoints:length(immig_rate)], Time = axis[removed_timepoints:length(axis)])
   
   Time <- NULL; rm(Time) # nolint, fixes warning: no visible binding for global variable
   Immigration <- NULL; rm(Immigration) # nolint, fixes warning: no visible binding for global variable
-  ggplot2::ggplot(data = immig_rate_time, ggplot2::aes(x = Time, y = Immigration)) +
+  plot(ggplot2::ggplot(data = immig_rate_time, ggplot2::aes(x = Time, y = Immigration)) +
     ggplot2::geom_line(size = 1) +
-    ggplot2::ylim(0, 10)
+    ggplot2::ylim(0, 0.01))
   invisible(immig_rate_time)
 }
 
